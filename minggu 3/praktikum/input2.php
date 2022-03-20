9381dba6e1339708fd85460a0bd65119695f9a42
upinreborn <42901637+upinreborn@users.noreply.github.com>
2022-03-20 14:12:24 +0700

    Create input2.php

---
+++ minggu 3/praktikum/input2.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title>Pengolahan Form</title>
+</head>
+
+<body>
+    <form action="proses02.php" method="post" name="input">
+        Nama Anda : <input type="text" name="nama"><br>
+        <input type="submit" name="Input" value="Input">
+    </form>
+</body>
+</html>
