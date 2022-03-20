2850844a00adaa4d3bbbc6cb16ad5528c2d5c68d
upinreborn <42901637+upinreborn@users.noreply.github.com>
2022-03-20 14:14:28 +0700

    Create inputcheckbox.php

---
+++ minggu 3/praktikum/inputcheckbox.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Band Favorit ~ Inputan Checkbox</title>
+</head>
+
+<body>
+    <FORM ACTION="prosescheckbox.php" METHOD="POST" NAME="input">
+        <h2>Pilih Band Favorit Anda :</h2>
+        <input type="checkbox" name="band01" value="A7x" checked>
+        A7x<br>
+        <input type="checkbox" name="band02" value="Sheila On7">
+        Sheila On 7<br>
+        <input type="checkbox" name="band03" value="One OK Rock">One OK Rock<br>
+        <input type="checkbox" name="band04" value="Ungu">Ungu<br>
+        <input type="submit" name="Pilih" value="Pilih">
+    </FORM>
+</body>
+</html>
