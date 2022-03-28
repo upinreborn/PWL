043a418a6d13d312584e290c3339f273dab5103c
upinreborn <42901637+upinreborn@users.noreply.github.com>
2022-03-28 23:08:34 +0700

    Add files via upload

---
+++ minggu 4/praktikum/array05.php
@@ -0,0 +1,8 @@
+<?
+$arrWarna = array("Blue", "Black", "Red", "Yellow", "Green");
+$arrNilai = array("Ani" => 80, "Otim" => 90, "Ana" => 75, "Budi" => 85);
+echo "<pre>";
+print($arrWarna);
+echo "<br>";
+print($arrNilai);
+echo "</pre>";
